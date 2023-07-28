feefad1d8d7a23bcc46d3cd461b156820f63c41e
sangsakaa <zainuri717@gmail.com>
2023-07-28 08:30:44 +0700

    laporan harian

--- app/Http/Controllers/SiacaController.php
+++ app/Http/Controllers/SiacaController.php
@@ -22,7 +22,7 @@ class SiacaController extends Controller
             ->leftJoin('sesi_laporan_harian', 'sesi_laporan_harian.anggota_kelompok_id', '=', 'anggota_kelompok.mahasiswa_id')
             ->leftJoin('laporan_mahasiswa', 'laporan_mahasiswa.sesi_laporan_harian_id', '=', 'sesi_laporan_harian.id')
             ->leftjoin('mahasiswa', 'mahasiswa.id', '=', 'anggota_kelompok.mahasiswa_id')
-        ->where('status_laporan', 'valid ')->orderby('nama_kelompok')->orderby('laporan_mahasiswa.updated_at');
+        ->where('status_laporan', 'valid ')->orderby('laporan_mahasiswa.updated_at');
         if (request('cari')) {
             $dataLap->where('nama_mhs', 'like', '%' . request('cari') . '%')->orderBy('nama_mhs');
         }
