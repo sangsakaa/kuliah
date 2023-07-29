67b804933b17986835ac66a777ca1d4757752b01
sangsakaa <zainuri717@gmail.com>
2023-07-30 05:22:22 +0700

    grafik bar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -88,6 +88,9 @@
                 </div>
                 @elseif (Auth::user()->hasRole('siaca'))
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Beranda') }}
+                    </x-nav-link>
                     <x-nav-link :href="route('cek-valid-dosen')" :active="request()->routeIs('cek-valid-dosen')">
                         {{ __('Cek Validasi Dosen') }}
                     </x-nav-link>
