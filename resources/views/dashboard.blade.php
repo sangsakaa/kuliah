0338c6881ae9a73f6b3026771920d045715cc0aa
sangsakaa <zainuri717@gmail.com>
2023-08-01 10:11:40 +0700

    orderby

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -277,190 +277,192 @@
                         });
                     </script>
                 </div>
-                @endrole
-                @role('dosen')
-                <div class=" grid grid-cols-1">
-                    <div>
-                        <div class=" w-full py-2 px-2  grid grid-cols-1 gap-2 sm:grid-cols-4">
-                            <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase">
-                                <a href="/sesi-validasi-laporan-mhs">
-                                    <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase">Val Laporan Mahasiswa</div>
-                                </a>
-                            </div>
-                            <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase">
-                                <a href="/data-anggota">
-                                    <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase"> Data Anggota</div>
-                                </a>
-                            </div>
-                            <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase">
-                                <a href="/supervisi-dosen">
-                                    <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase"> Supervisi</div>
-                                </a>
-                            </div>
-                            <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase">
-                                <a href="/daftar-nilai">
-                                    <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase"> Nilai</div>
-                                </a>
-                            </div>
-                            <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase">
-                                <a href="/time-line">
-                                    <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase"> Time Line</div>
-                                </a>
-                            </div>
-                            <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase">
-                                <a href="/time-line">
-                                    <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase"> PKM</div>
-                                </a>
-                            </div>
-                        </div>
+            </div>
+        </div>
+        @endrole
+        @role('dosen')
+        <div class=" grid grid-cols-1">
+            <div>
+                <div class=" w-full py-2 px-2  grid grid-cols-1 gap-2 sm:grid-cols-4">
+                    <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase">
+                        <a href="/sesi-validasi-laporan-mhs">
+                            <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase">Val Laporan Mahasiswa</div>
+                        </a>
                     </div>
-                    <div class=" w-full">
-                        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-                        <canvas id="statusChart" width="400" height="200"></canvas>
-                        <script>
-                            // Assuming $statusChartData contains the data for the chart
-
-                            // Get the canvas element
-                            const statusChartCanvas = document.getElementById('statusChart');
-
-                            // Create the bar chart
-                            const statusChart = new Chart(statusChartCanvas, {
-                                type: 'bar',
-                                data: {
-                                    labels: @json($statusChartData['labels']),
-                                    datasets: [{
-                                        label: 'Status Laporan',
-                                        data: @json($statusChartData['data']),
-                                        backgroundColor: [
-                                            'rgba(255, 99, 132, 0.2)', // Menunggu - Red
-                                            'rgba(54, 162, 235, 0.2)', // Valid - Blue
-                                            'rgba(255, 206, 86, 0.2)', // Draf - Yellow
-                                        ],
-                                        borderColor: [
-                                            'rgba(255, 99, 132, 1)',
-                                            'rgba(54, 162, 235, 1)',
-                                            'rgba(255, 206, 86, 1)',
-                                        ],
-                                        borderWidth: 1
-                                    }]
-                                },
-                                options: {
-                                    scales: {
-                                        y: {
-                                            beginAtZero: true
-                                        }
-                                    }
-                                }
-                            });
-                        </script>
-
+                    <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase">
+                        <a href="/data-anggota">
+                            <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase"> Data Anggota</div>
+                        </a>
                     </div>
-                </div>
-                @endrole
-                @role('siaca')
-                <div class=" w-full py-2 px-2  grid grid-cols-1 gap-2 uppercase text-xs ">
-                    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-                    <canvas id="grafikStatusLaporan" style="width: 600px; height: 400px;"></canvas>
-                    <script>
-                        // Data jumlah status_laporan dari PHP
-                        var dataLaporan = <?php echo json_encode($jumlahStatusLaporan); ?>;
-
-                        // Mengambil nama-nama dosen sebagai label grafik
-                        var namaDosen = Object.keys(dataLaporan);
-
-                        // Mengambil data jumlah status_laporan valid, menunggu, dan draf untuk setiap dosen
-                        var jumlahValid = [];
-                        var jumlahMenunggu = [];
-                        var jumlahDraf = [];
-
-                        namaDosen.forEach(function(nama) {
-                            jumlahValid.push(dataLaporan[nama].valid);
-                            jumlahMenunggu.push(dataLaporan[nama].menunggu);
-                            jumlahDraf.push(dataLaporan[nama].draf);
-                        });
-
-                        // Membuat grafik bar
-                        var ctx = document.getElementById('grafikStatusLaporan').getContext('2d');
-                        var myChart = new Chart(ctx, {
-                            type: 'bar',
-                            data: {
-                                labels: namaDosen,
-                                datasets: [{
-                                    label: 'Valid',
-                                    data: jumlahValid,
-                                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                                    borderColor: 'rgba(75, 192, 192, 1)',
-                                    borderWidth: 1
-                                }, {
-                                    label: 'Menunggu',
-                                    data: jumlahMenunggu,
-                                    backgroundColor: 'rgba(255, 206, 86, 0.2)',
-                                    borderColor: 'rgba(255, 206, 86, 1)',
-                                    borderWidth: 1
-                                }, {
-                                    label: 'Draf',
-                                    data: jumlahDraf,
-                                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                                    borderColor: 'rgba(255, 99, 132, 1)',
-                                    borderWidth: 1
-                                }]
-                            },
-                            options: {
-                                indexAxis: 'y', // Display labels on the right side of the x-axis
-                                scales: {
-                                    y: {
-                                        beginAtZero: true
-                                    }
-                                }
-                            }
-                        });
-                    </script>
-                    <div>
-                        <!-- Tambahkan library Chart.js -->
-                        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-                        <!-- Buat elemen canvas untuk menampilkan grafik -->
-                        <canvas id="grafikLaporan"></canvas>
-
-                        <!-- Script untuk inisialisasi grafik -->
-                        <script>
-                            var ctx = document.getElementById('grafikLaporan').getContext('2d');
-                            var data = @json($data);
-                            var labels = @json($labels);
-                            var statusColors = data.map(function(value) {
-                                return value === 0 ? 'rgba(75, 192, 192, 0.2)' : 'rgba(75, 192, 192, 0.2)';
-                            });
-
-                            var myChart = new Chart(ctx, {
-                                type: 'bar',
-                                data: {
-                                    labels: labels, // Placed on the x-axis (horizontal axis)
-                                    datasets: [{
-                                        label: 'Jumlah Laporan Valid',
-                                        data: data, // Placed on the y-axis (vertical axis)
-                                        backgroundColor: statusColors,
-                                        borderColor: statusColors.map(function(color) {
-                                            return color.replace('0.2', '1');
-                                        }),
-                                        borderWidth: 1
-                                    }]
-                                },
-                                options: {
-                                    scales: {
-                                        x: {
-                                            position: 'right', // Display x-axis labels on the right side
-                                            beginAtZero: true
-                                        },
-                                        y: { // Configure the y-axis (vertical axis)
-                                            beginAtZero: true
-                                        }
-                                    }
-                                }
-                            });
-                        </script>
+                    <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase">
+                        <a href="/supervisi-dosen">
+                            <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase"> Supervisi</div>
+                        </a>
+                    </div>
+                    <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase">
+                        <a href="/daftar-nilai">
+                            <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase"> Nilai</div>
+                        </a>
+                    </div>
+                    <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase">
+                        <a href="/time-line">
+                            <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase"> Time Line</div>
+                        </a>
+                    </div>
+                    <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase">
+                        <a href="/time-line">
+                            <div class=" w-full bg-blue-800 px-2 py-1 text-white text-center uppercase"> PKM</div>
+                        </a>
                     </div>
                 </div>
             </div>
-            @endrole
+            <div class=" w-full">
+                <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+                <canvas id="statusChartDosen" width="400" height="200"></canvas>
+                <script>
+                    // Assuming $statusChartData contains the data for the chart
+
+                    // Get the canvas element
+                    const statusChartCanvas = document.getElementById('statusChartDosen');
+
+                    // Create the bar chart
+                    const statusChart = new Chart(statusChartCanvas, {
+                        type: 'bar',
+                        data: {
+                            labels: @json($statusChartDataDosen['labels']),
+                            datasets: [{
+                                label: 'Status Laporan',
+                                data: @json($statusChartDataDosen['data']),
+                                backgroundColor: [
+                                    'rgba(255, 99, 132, 0.2)', // Menunggu - Red
+                                    'rgba(54, 162, 235, 0.2)', // Valid - Blue
+                                    'rgba(255, 206, 86, 0.2)', // Draf - Yellow
+                                ],
+                                borderColor: [
+                                    'rgba(255, 99, 132, 1)',
+                                    'rgba(54, 162, 235, 1)',
+                                    'rgba(255, 206, 86, 1)',
+                                ],
+                                borderWidth: 1
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                y: {
+                                    beginAtZero: true
+                                }
+                            }
+                        }
+                    });
+                </script>
+
+            </div>
         </div>
+        @endrole
+        @role('siaca')
+        <div class=" w-full py-2 px-2  grid grid-cols-1 gap-2 uppercase text-xs ">
+            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+            <canvas id="grafikStatusLaporan" style="width: 600px; height: 400px;"></canvas>
+            <script>
+                // Data jumlah status_laporan dari PHP
+                var dataLaporan = <?php echo json_encode($jumlahStatusLaporan); ?>;
+
+                // Mengambil nama-nama dosen sebagai label grafik
+                var namaDosen = Object.keys(dataLaporan);
+
+                // Mengambil data jumlah status_laporan valid, menunggu, dan draf untuk setiap dosen
+                var jumlahValid = [];
+                var jumlahMenunggu = [];
+                var jumlahDraf = [];
+
+                namaDosen.forEach(function(nama) {
+                    jumlahValid.push(dataLaporan[nama].valid);
+                    jumlahMenunggu.push(dataLaporan[nama].menunggu);
+                    jumlahDraf.push(dataLaporan[nama].draf);
+                });
+
+                // Membuat grafik bar
+                var ctx = document.getElementById('grafikStatusLaporan').getContext('2d');
+                var myChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: namaDosen,
+                        datasets: [{
+                            label: 'Valid',
+                            data: jumlahValid,
+                            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                            borderColor: 'rgba(75, 192, 192, 1)',
+                            borderWidth: 1
+                        }, {
+                            label: 'Menunggu',
+                            data: jumlahMenunggu,
+                            backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                            borderColor: 'rgba(255, 206, 86, 1)',
+                            borderWidth: 1
+                        }, {
+                            label: 'Draf',
+                            data: jumlahDraf,
+                            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                            borderColor: 'rgba(255, 99, 132, 1)',
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        indexAxis: 'y', // Display labels on the right side of the x-axis
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
+            </script>
+            <div>
+                <!-- Tambahkan library Chart.js -->
+                <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+                <!-- Buat elemen canvas untuk menampilkan grafik -->
+                <canvas id="grafikLaporan"></canvas>
+
+                <!-- Script untuk inisialisasi grafik -->
+                <script>
+                    var ctx = document.getElementById('grafikLaporan').getContext('2d');
+                    var data = @json($data);
+                    var labels = @json($labels);
+                    var statusColors = data.map(function(value) {
+                        return value === 0 ? 'rgba(75, 192, 192, 0.2)' : 'rgba(75, 192, 192, 0.2)';
+                    });
+
+                    var myChart = new Chart(ctx, {
+                        type: 'bar',
+                        data: {
+                            labels: labels, // Placed on the x-axis (horizontal axis)
+                            datasets: [{
+                                label: 'Jumlah Laporan Valid',
+                                data: data, // Placed on the y-axis (vertical axis)
+                                backgroundColor: statusColors,
+                                borderColor: statusColors.map(function(color) {
+                                    return color.replace('0.2', '1');
+                                }),
+                                borderWidth: 1
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                x: {
+                                    position: 'right', // Display x-axis labels on the right side
+                                    beginAtZero: true
+                                },
+                                y: { // Configure the y-axis (vertical axis)
+                                    beginAtZero: true
+                                }
+                            }
+                        }
+                    });
+                </script>
+            </div>
+        </div>
+    </div>
+    @endrole
+    </div>
 </x-app-layout>
