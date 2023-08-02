eef27dca85f5468730997cead603f71c5db0f3c3
sangsakaa <zainuri717@gmail.com>
2023-08-02 13:48:06 +0700

    dashboar dosen

--- resources/views/admin/siaca/checkLap/score.blade.php
+++ resources/views/admin/siaca/checkLap/score.blade.php
@@ -9,28 +9,38 @@
       <div class=" p-2">
         <div class="container">
           <h2>Hasil Perhitungan Status Laporan</h2>
+          @php
+          // Ubah array menjadi objek koleksi Laravel
+          $statusCounts = collect($statusCounts);
+          // Urutkan objek koleksi berdasarkan perbedaan valid dan menunggu
+          $sortedStatusCounts = $statusCounts->sortByDesc(function($statusCount) {
+          return $statusCount['valid'] - $statusCount['menunggu'];
+          });
+          @endphp
+
           <table class="table w-full">
             <thead>
               <tr>
-                <th class=" border px-1">No</th>
-                <th class=" border px-1">Dosen</th>
-                <th class=" border px-1">Jumlah Valid</th>
-                <th class=" border px-1">Jumlah Menunggu</th>
-                <th class=" border px-1">Score</th>
+                <th class="border px-1">No</th>
+                <th class="border px-1">Dosen</th>
+                <th class="border px-1">Jumlah Valid</th>
+                <th class="border px-1">Jumlah Menunggu</th>
+                <th class="border px-1">Score</th>
               </tr>
             </thead>
             <tbody>
-              @foreach ($statusCounts as $statusCount)
+              @foreach ($sortedStatusCounts as $statusCount)
               <tr>
-                <td class=" border px-1 text-left">{{ $loop->iteration }}</td>
-                <td class=" border px-1 text-left">{{ $statusCount['dosen'] }}</td>
-                <td class=" border px-1 text-center">{{ $statusCount['valid'] }}</td>
-                <td class=" border px-1 text-center">{{ $statusCount['menunggu'] }}</td>
-                <td class=" border px-1 text-center">{{ $statusCount['valid'] - $statusCount['menunggu']  }}</td>
+                <td class="border px-1 text-left">{{ $loop->iteration }}</td>
+                <td class="border px-1 text-left">{{ $statusCount['dosen'] }}</td>
+                <td class="border px-1 text-center">{{ $statusCount['valid'] }}</td>
+                <td class="border px-1 text-center">{{ $statusCount['menunggu'] }}</td>
+                <td class="border px-1 text-center">{{ $statusCount['valid'] - $statusCount['menunggu'] }}</td>
               </tr>
               @endforeach
             </tbody>
           </table>
+
         </div>
       </div>
     </div>
