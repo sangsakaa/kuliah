7889f72dcdced06c8b2ff50e8cd843387b79bda9
sangsakaa <zainuri717@gmail.com>
2023-08-06 17:25:45 +0700

    ok

--- resources/views/admin/siaca/checkLap/scoremhs.blade.php
+++ resources/views/admin/siaca/checkLap/scoremhs.blade.php
@@ -28,7 +28,7 @@
       // echo "Total Menunggu: " . $totalMenunggu; // Cetak total 'menunggu'
       // echo "Total Valid: " . $totalValid; // Cetak total 'menunggu'
       ?>
-      <div class=" grid grid-cols-3  sm:flex sm:grid-cols-3 gap-2">
+      <div class=" grid grid-cols-1 sm:flex sm:grid-cols-3 gap-2">
         <div class=" bg-red-600 p-2 grid grid-cols-2 rounded-md">
           <div class=" flex">
             <span class=" text-white">Menunggu </span>
@@ -37,12 +37,9 @@
             <span class="text-3xl text-white">
               {{$totalMenunggu}}
             </span> <span class=" text-white">
-
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
               </svg>
-
-
             </span>
 
           </div>
