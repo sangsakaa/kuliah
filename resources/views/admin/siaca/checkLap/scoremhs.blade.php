0b08ce93e971f1cd7fd5dee0476f85229d325ab8
sangsakaa <zainuri717@gmail.com>
2023-08-06 10:40:40 +0700

    dashboard

--- resources/views/admin/siaca/checkLap/scoremhs.blade.php
+++ resources/views/admin/siaca/checkLap/scoremhs.blade.php
@@ -29,14 +29,14 @@
       // echo "Total Valid: " . $totalValid; // Cetak total 'menunggu'
       ?>
       <div class=" grid sm:grid grid-cols-1 sm:grid-cols-8 gap-2">
-        <div class=" bg-red-700 p-2 grid grid-cols-2 rounded-md">
+        <div class=" bg-red-600 p-2 grid grid-cols-2 rounded-md">
           <div class=" flex">
-            <span>Menunggu </span>
+            <span class=" text-white">Menunggu </span>
           </div>
           <div class="  flex justify-end">
-            <span class="text-3xl">
+            <span class="text-3xl text-white">
               {{$totalMenunggu}}
-            </span> <span>
+            </span> <span class=" text-white">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
               </svg>
