5c01cdbcfe37221860ce5a22962bbd2408a059ec
sangsakaa <zainuri717@gmail.com>
2023-08-09 16:51:50 +0700

    fix

--- app/Http/Controllers/KualisLapController.php
+++ app/Http/Controllers/KualisLapController.php
@@ -46,7 +46,7 @@ class KualisLapController extends Controller
             ->whereIn('laporan_mahasiswa.status_laporan', ['draf', 'valid', 'menunggu']) // Ubah "status_laporan" yang valid dan menunggu
             ->orderBy('nama_kelompok')
             ->where('kelompok.dosen_id', $UserPerDosen)
-            ->whereNull('laporan_mahasiswa.kualitas_lap')
+            // ->whereNull('laporan_mahasiswa.kualitas_lap')
             ->paginate(2);
 
 
