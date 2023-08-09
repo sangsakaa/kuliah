027b0217be66c70392f2366ddbb7872ba6145560
sangsakaa <zainuri717@gmail.com>
2023-08-10 04:07:07 +0700

    test

--- resources/views/admin/siaca/checkLap/laporan.blade.php
+++ resources/views/admin/siaca/checkLap/laporan.blade.php
@@ -27,7 +27,7 @@
                   <th class=" border px-1 ">No</th>
                   <th class=" border px-1 w-1/3  ">Bukti Laporan</th>
                   <th class=" border px-1 w-1/3 ">Detail Lap</th>
-                  <th class=" border px-1  w-1/3 ">Status</th>
+                  <th class=" border px-1  w-1/4 ">Status</th>
                 </tr>
               </thead>
               <tbody>
