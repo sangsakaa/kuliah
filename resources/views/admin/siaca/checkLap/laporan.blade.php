92882d32b9fa69cba1e07b02c9b27ce48eef8e4c
sangsakaa <zainuri717@gmail.com>
2023-08-10 04:05:34 +0700

    test

--- resources/views/admin/siaca/checkLap/laporan.blade.php
+++ resources/views/admin/siaca/checkLap/laporan.blade.php
@@ -20,14 +20,14 @@
             </div>
           </div>
           <div class=" ">
-            <table class=" mt-2 w-full">
+            <table class=" mt-2 w-fit ">
               <thead>
                 <tr>
                   <!-- <th class=" border px-1 w-3 ">ID</th> -->
                   <th class=" border px-1 ">No</th>
                   <th class=" border px-1 w-1/3  ">Bukti Laporan</th>
                   <th class=" border px-1 w-1/3 ">Detail Lap</th>
-                  <th class=" border px-1 ">Status</th>
+                  <th class=" border px-1 w-fit ">Status</th>
                 </tr>
               </thead>
               <tbody>
