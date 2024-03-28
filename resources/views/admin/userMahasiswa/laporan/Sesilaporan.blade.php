241caaec8dfc489ea39cf3b6498df23cf405ea34
sangsakaa <zainuri717@gmail.com>
2024-03-28 12:06:25 +0700

    update layout

--- resources/views/admin/userMahasiswa/laporan/Sesilaporan.blade.php
+++ resources/views/admin/userMahasiswa/laporan/Sesilaporan.blade.php
@@ -106,95 +106,46 @@
   <div class=" py-1 mt-2 bg-white">
     <div class=" overflow-auto p-4">
       <div>
-        <table hidden class="w-full border border-green-700">
-          <thead>
-            <tr>
-              <th class="border border-green-700 px-2 py-1 w-5 sm:w-3">No</th>
-              <th class="border border-green-700 px-2 py-1">Tanggal</th>
-              <th class="border border-green-700 px-2 py-1">Jam </th>
-              <th class="border border-green-700 px-2 py-1">updated_at </th>
-              <th class="border border-green-700 px-2 py-1">Laporan</th>
-              <th class="border border-green-700 px-2 py-1">Status</th>
-              <th class="border border-green-700 px-2 py-1">Catatan Pembimbing</th>
-            </tr>
-          </thead>
-          <tbody>
-            @if($DataSesiLap->count() != null)
-            @foreach($DataSesiLap as $list)
-            <tr class=" text-xs sm:text-sm">
-              <td class="border border-green-700 px-2 py-1 text-center">{{$loop->iteration}}</td>
-              <td class="border border-green-700 px-2 py-1 text-center">
-                {{ \Carbon\Carbon::parse($list->tanggal)->isoFormat('dddd , DD MMMM Y') }}
-              </td>
-              <td class="border border-green-700 px-2 py-1 text-center">
-                {{ \Carbon\Carbon::parse($list->created_at)->isoFormat('H:m') }}
-              </td>
-              <td class="border border-green-700 px-2 py-1 text-center">
-                @php
-                // Ambil waktu sekarang dalam timezone tertentu, misalnya 'Asia/Jakarta'
-                $now = \Carbon\Carbon::now('Asia/Jakarta');
 
-                // Parse waktu dari database atau sumber lainnya ke dalam objek Carbon
-                $created_at = \Carbon\Carbon::parse($list->created_at);
-
-                // Hitung selisih waktu antara waktu sekarang dan waktu dari database
-                $diff = $created_at->diffForHumans($now);
-                @endphp
-                <p> {{ $diff }}</p>
-              </td>
-              <td class="border border-green-700 px-2 py-1 text-center">
-                <a href="/laporan-mahasiswa/{{$list->id}}">Laporan </a>
-              </td>
-              <td class="border border-green-700 px-2 py-1 text-center capitalize">
-                @foreach($list->laporanMahasiswa as $status)
-                @if($status->status_laporan === 'menunggu')
-                <span class="text-red-700 font-semibold">{{$status->status_laporan}}</span>
-                @elseif($status->status_laporan === 'valid')
-                <span class="text-green-700 font-semibold">{{$status->status_laporan}}</span>
-                @elseif($status->status_laporan === null)
-                <span class="text-black">Belum melakukan laporan</span>
-                @else
-                {{$status->status_laporan}}
-                @endif
-                @endforeach
-
-                @if(count($list->laporanMahasiswa) === 0)
-                <span class="text-black">Belum Laporan</span>
-                @endif
-              </td>
-              <td class=" border border-green-700 px-2 py-1">
-                @foreach($list->laporanMahasiswa as $status)
-                {{$status->note_laporan}}
-                @endforeach
-              </td>
-            </tr>
-            @endforeach
-            @else
-            <tr>
-              <td class=" text-red-700 text-center py-1 font-semibold uppercase" colspan="6">tidak ada sesi Laporan</td>
-            </tr>
-            @endif
-            <!-- Tambahkan baris lainnya di sini -->
-          </tbody>
-        </table>
         @if($DataSesiLap->count() != null)
+        @foreach($DataSesiLap as $list)
         <a href="/laporan-mahasiswa/{{$list->id}}">
-          <div class="  flex gap-2 ">
-            <div class=" bg-blue-200 rounded-md ">
-              <div class="flex justify-center items-center justify-items-center">
-                <span class="grid justify-center items-center justify-items-center flex">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-blue-500 w-20 h-20">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
-                  </svg>
-                </span>
-              </div>
+          <div class=" flex grid-cols-2 gap-2 ">
+            <div class="   justify-center  items-center grid  bg-blue-200 rounded-md  w-16 ">
+              @foreach($list->laporanMahasiswa as $status)
+              @if($status->status_laporan === 'menunggu')
+              <span class="text-red-700 font-semibold">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                </svg>
+              </span>
+              @elseif($status->status_laporan === 'valid')
+              <span class="text-green-700 font-semibold">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                </svg>
+
+              </span>
+              @elseif($status->status_laporan === null)
+              <span class="text-black">
+                Belum melakukan laporan
+              </span>
+              @else
+              {{$status->status_laporan}}
+              @endif
+              @endforeach
+              @if(count($list->laporanMahasiswa) === 0)
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12 text-red-700">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+              </svg>
+
+              @endif
             </div>
-            <div class=" bg-blue-200 w-full rounded-md">
+            <div class=" bg-blue-200  rounded-md w-full">
               @if($DataSesiLap->count() != null)
               @foreach($DataSesiLap as $list)
               <div class=" p-2">
                 <span class=" text-xs sm:text-sm">
-
                   <td class="border border-green-700 px-2 py-1 text-center">
                     {{ \Carbon\Carbon::parse($list->tanggal)->isoFormat('dddd , DD MMMM Y') }}
                   </td>
@@ -213,8 +164,9 @@
                     $diff = $created_at->diffForHumans($now);
                     @endphp
                     <p> {{ $diff }}</p>
+
                   </span>
-                  <span class="">
+                  <span hidden class="">
                     <span class=" font-semibold capitalize">
                       @foreach($list->laporanMahasiswa as $status)
                       @if($status->status_laporan === 'menunggu')
@@ -246,6 +198,18 @@
               </div>
             </div>
           </div>
+          <div class="py-2 ">
+            @foreach($list->laporanMahasiswa as $status)
+            <div class=" py-2 px-2 bg-yellow-500">
+              <span>
+
+                Note : {{$status->note_laporan}}
+
+              </span>
+            </div>
+            @endforeach
+          </div>
+          @endforeach
         </a>
         @else
         <div class=" grid justify-center">
