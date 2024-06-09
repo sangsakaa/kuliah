9b37f90b8bbcad976b025f9c35b35e1476cc8681
sangsaka <zainuri717@gmail>
2024-06-09 15:29:44 +0700

    ok

--- config/app.php
+++ config/app.php
@@ -225,7 +225,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
-        Barryvdh\DomPDF\ServiceProvider::class,
+        
         
         
         
