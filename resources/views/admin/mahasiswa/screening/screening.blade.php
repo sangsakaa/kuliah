6452de89753cf8f36e5263f433e4902f6780174e
sangsaka <zainuri717@gmail>
2024-06-12 09:02:48 +0700

    ok

--- resources/views/admin/mahasiswa/screening/screening.blade.php
+++ resources/views/admin/mahasiswa/screening/screening.blade.php
@@ -27,119 +27,134 @@
             @else
             <div class="  justify-items-center grid grid-cols-1">
               <p>data sudah di temukan</p>
+              <div>
+                <div class=" px-6">
+                  @if (session('success'))
+                  <div class=" bg-green-400 text-white px-4 py-2">
+                    {{ session('success') }}
+                  </div>
+                  @endif
+
+                  <!-- Notifikasi Error -->
+                  @if (session('error'))
+                  <div class=" bg-red-500 text-white px-6 py-2">
+                    {{ session('error') }}
+                  </div>
+                  @endif
+                </div>
+              </div>
             </div>
+            @endif
+            @endforeach
+            @endif
           </div>
-          @endif
-          @endforeach
-          @endif
         </div>
       </div>
     </div>
-  </div>
-  <form action="/screening-mahasiswa-jawab" method="post">
-    @csrf
-    <div class="w-full py-2 px-2">
-      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-        @if(!$mahasiswa->isEmpty())
-        <div class="w-full py-4 px-4 gap-2">
-          <div class="grid">
+    <form action="/screening-mahasiswa-jawab" method="post">
+      @csrf
+      <div class="w-full py-2 px-2">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+          @if(!$mahasiswa->isEmpty())
+          <div class="w-full py-4 px-4 gap-2">
             <div class="grid">
-              @foreach($mahasiswa as $detail)
-              @if($detail->id == request('cari') || is_null(request('cari')))
-              @else
-              <span class="font-semibold">1. Data Pribadi</span>
-              <div class="grid grid-cols-1 sm:grid-cols-2">
-                <div class="flex">
-                  <div class="w-16 ">
-                    NIM
+              <div class="grid">
+                @foreach($mahasiswa as $detail)
+                @if($detail->id == request('cari') || is_null(request('cari')))
+                @else
+                <span class="font-semibold">1. Data Pribadi</span>
+                <div class="grid grid-cols-1 sm:grid-cols-2">
+                  <div class="flex">
+                    <div class="w-16 ">
+                      NIM
+                    </div>
+                    <div class="">
+                      : @if($detail->nim != null)
+                      {{$detail->nim}}
+                      @else
+                      <span class="text-red-600 font-semibold capitalize"> belum ada nis </span>
+                      @endif
+                    </div>
                   </div>
-                  <div class="">
-                    : @if($detail->nim != null)
-                    {{$detail->nim}}
-                    @else
-                    <span class="text-red-600 font-semibold capitalize"> belum ada nis </span>
-                    @endif
+                  <div class="flex">
+                    <div class="w-16 ">
+                      Nama
+                    </div>
+                    <div class="">
+                      : {{$detail->nama_mhs}} <br>
+                      <input hidden type="text" name="mahasiswa_id" value="{{$detail->id}}" class="w-full border border-gray-300 p-2">
+                    </div>
                   </div>
-                </div>
-                <div class="flex">
-                  <div class="w-16 ">
-                    Nama
+                  <div class="flex">
+                    <div class="w-16 ">
+                      Prodi
+                    </div>
+                    <div class="">
+                      :
+                      {{$detail->prodi}}
+                      <input hidden type="text" name="mahasiswa_id" value="{{$detail->id}}" class="w-full border border-gray-300 p-2">
+                    </div>
                   </div>
-                  <div class="">
-                    : {{$detail->nama_mhs}} <br>
-                    <input hidden type="text" name="mahasiswa_id" value="{{$detail->id}}" class="w-full border border-gray-300 p-2">
+                  <div class="flex">
+                    <div class="w-16 ">
+                      Tgl Lahir
+                    </div>
+                    <div class=" capitalize">
+                      : {{ \Carbon\Carbon::parse($detail->tgl_lahir)->isoFormat(' DD MMMM Y') }} | <?php
+                                                                                                    $dateOfBirth = \Carbon\Carbon::parse($detail->tgl_lahir);
+                                                                                                    $age = $dateOfBirth->age;
+                                                                                                    echo "$age"
+                                                                                                    ?>
+                    </div>
                   </div>
-                </div>
-                <div class="flex">
-                  <div class="w-16 ">
-                    Prodi
-                  </div>
-                  <div class="">
-                    :
-                    {{$detail->prodi}}
-                    <input hidden type="text" name="mahasiswa_id" value="{{$detail->id}}" class="w-full border border-gray-300 p-2">
-                  </div>
-                </div>
-                <div class="flex">
-                  <div class="w-16 ">
-                    Tgl Lahir
-                  </div>
-                  <div class=" capitalize">
-                    : {{ \Carbon\Carbon::parse($detail->tgl_lahir)->isoFormat(' DD MMMM Y') }} | <?php
-                                                                                                  $dateOfBirth = \Carbon\Carbon::parse($detail->tgl_lahir);
-                                                                                                  $age = $dateOfBirth->age;
-                                                                                                  echo "$age"
-                                                                                                  ?>
-                  </div>
-                </div>
 
+                </div>
+                <div class=" overflow-auto">
+                  <span class="font-semibold">2. Kondisi Khusus Peserta</span>
+                  <table class="w-full">
+                    <thead>
+                      <tr class=" border bg-gray-50">
+                        <th>Pertanyaan</th>
+                        <th>Jawaban</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      @foreach($soal as $list)
+                      <tr class=" border">
+                        <td class=" bg-gray-200 border px-2">
+                          {{$list->soal}}
+                          <input type="text" hidden name="screening_id[]" value="{{$list->id}}" class="w-full border border-gray-300 p-2">
+                        </td>
+                        <td class=" border ">
+                          <input required type="radio" name="jawaban[{{$list->id}}]" value="ya" class=" border border-gray-300 p-2" {{isset($jawaban[$list->id]) ? $jawaban[$list->id]->jawaban == 'ya' ? 'checked' : '' : ''}}>
+                          <label for="">Ya</label> <br>
+                          <input required type="radio" name="jawaban[{{$list->id}}]" value="tidak" class=" border border-gray-300 p-2" {{isset($jawaban[$list->id]) ? $jawaban[$list->id]->jawaban == 'tidak' ? 'checked' : '' : ''}}>
+                          <label for="">Tidak</label>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td class=" border" colspan="3">
+                          <input type="text" placeholder="jika (YA) sebutkan" name="keterangan[{{$list->id}}]" class="w-full border border-gray-300 p-2" value="{{ isset($jawaban[$list->id]) ? $jawaban[$list->id]->keterangan : '' }}">
+                        </td>
+                      </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
+                  <button type="submit" class="mt-4 bg-blue-500 text-white p-2 rounded">Submit</button>
+                  <a target="_blank" class=" bg-red-600  dark:bg-purple-600 py-2  rounded-sm hover:bg-purple-600 text-white px-4 " href="pdf/screen/{{$mahasiswa->first()->nim}}">Cetak Kartu Kesehatan</a>
+                  </table>
+                </div>
+                <div class=" overflow-auto">
+                </div>
+                @endif
+                @endforeach
               </div>
-              <div class=" overflow-auto">
-                <span class="font-semibold">2. Kondisi Khusus Peserta</span>
-                <table class="w-full">
-                  <thead>
-                    <tr class=" border bg-gray-50">
-                      <th>Pertanyaan</th>
-                      <th>Jawaban</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    @foreach($soal as $list)
-                    <tr class=" border">
-                      <td class=" bg-gray-200 border px-2">
-                        {{$list->soal}}
-                        <input type="text" hidden name="screening_id[]" value="{{$list->id}}" class="w-full border border-gray-300 p-2">
-                      </td>
-                      <td class=" border ">
-                        <input required type="radio" name="jawaban[{{$list->id}}]" value="ya" class=" border border-gray-300 p-2" {{isset($jawaban[$list->id]) ? $jawaban[$list->id]->jawaban == 'ya' ? 'checked' : '' : ''}}>
-                        <label for="">Ya</label> <br>
-                        <input required type="radio" name="jawaban[{{$list->id}}]" value="tidak" class=" border border-gray-300 p-2" {{isset($jawaban[$list->id]) ? $jawaban[$list->id]->jawaban == 'tidak' ? 'checked' : '' : ''}}>
-                        <label for="">Tidak</label>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td class=" border" colspan="3">
-                        <input type="text" placeholder="jika (YA) sebutkan" name="keterangan[{{$list->id}}]" class="w-full border border-gray-300 p-2" value="{{ isset($jawaban[$list->id]) ? $jawaban[$list->id]->keterangan : '' }}">
-                      </td>
-                    </tr>
-                    @endforeach
-                  </tbody>
-                </table>
-                <button type="submit" class="mt-4 bg-blue-500 text-white p-2 rounded">Submit</button>
-                <a target="_blank" class=" bg-red-600  dark:bg-purple-600 py-2  rounded-sm hover:bg-purple-600 text-white px-4 " href="pdf/screen/{{$mahasiswa->first()->nim}}">Cetak Kartu Kesehatan</a>
-                </table>
-              </div>
-              <div class=" overflow-auto">
-              </div>
-              @endif
-              @endforeach
             </div>
           </div>
-        </div>
-        @endif
+          @endif
 
-  </form>
-  <div>
+    </form>
+
     @if($mahasiswa->isEmpty())
     <p>No data available.</p>
     @else
