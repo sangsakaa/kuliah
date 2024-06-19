b5932493c1d459a8643ab8c009bc7213b75cb93e
sangsaka <zainuri717@gmail>
2024-06-19 11:31:01 +0700

    update validasi file

--- app/Http/Controllers/ScreeningController.php
+++ app/Http/Controllers/ScreeningController.php
@@ -24,9 +24,9 @@ class ScreeningController extends Controller
                 'prodi',
                 'nim', 'file', 'status_file', 'file_screening.id as idfile'
         ])
-            ->orderByRaw("FIELD(status_file, 'Invalid', '') DESC")
-            ->orderByRaw("CASE WHEN file = 'Invalid' THEN 1 ELSE 2 END, file DESC")
-            ->orderByRaw("FIELD(prodi, 'S1 Akuntansi', 'S1 Manajemen', 'S1 Hukum Keluarga Islam (Ahwal Syakhshiyyah)', 'S1 Pendidikan Bahasa Inggris', 'S1 Pendidikan Matematika', 'S1 Pendidikan Kimia') DESC")
+            
+            // ->orderByRaw("CASE WHEN file = 'Invalid' THEN 1 ELSE 2 END, file DESC")
+            // ->orderByRaw("FIELD(prodi, 'S1 Akuntansi', 'S1 Manajemen', 'S1 Hukum Keluarga Islam (Ahwal Syakhshiyyah)', 'S1 Pendidikan Bahasa Inggris', 'S1 Pendidikan Matematika', 'S1 Pendidikan Kimia') DESC")
         ->get();
         // Mengelompokkan data berdasarkan mahasiswa_id
         $groupedData = $dataScreening
