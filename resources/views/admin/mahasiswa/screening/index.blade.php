a77daf1dbd43ea3e4e91e793542219d148b7621f
sangsaka <zainuri717@gmail>
2024-06-19 10:33:58 +0700

    update validasi file

--- resources/views/admin/mahasiswa/screening/index.blade.php
+++ resources/views/admin/mahasiswa/screening/index.blade.php
@@ -140,7 +140,7 @@
                   </span>
                   @else
                   <!-- Display a message indicating the file has not been uploaded -->
-                  <span class=" text-green-700 bg-green-800">
+                  <span class=" text-green-700">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M5 19a2 2 0 01-2-2V7a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1M5 19h14a2 2 0 002-2v-5a2 2 0 00-2-2H9a2 2 0 00-2 2v5a2 2 0 01-2 2z" />
                     </svg>
@@ -159,10 +159,12 @@
                       </svg>
                     </button>
                   </form>
-                  <a href="/update-validasi-pendaftaran/{{$data[0]->idfile}}" class=" py-1 px-2  bg-green-600 text-white " title="Validasi File">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
-                    </svg>
+                  <a href="/update-validasi-pendaftaran/{{$data[0]->idfile}}" class=" py-1 px-2  bg-blue-700 text-white " title="Validasi File">
+                    <span class=" bg-green-600 py-1">
+                      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                      </svg>
+                    </span>
                   </a>
                   <form action="/hapus-data-file/{{$data[0]->idfile}}" method="post">
                     @csrf
