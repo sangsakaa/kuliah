e79f88fdb0dd6beeeb3333cc71bbcd696da620a9
sangsaka <zainuri717@gmail>
2024-06-19 11:35:02 +0700

    update validasi file

--- app/Http/Controllers/ScreeningController.php
+++ app/Http/Controllers/ScreeningController.php
@@ -24,7 +24,7 @@ class ScreeningController extends Controller
                 'prodi',
                 'nim', 'file', 'status_file', 'file_screening.id as idfile'
         ])
-            ->orderby('file', 'desc')          
+            ->orderby('file', 'asc')          
         ->get();
         // Mengelompokkan data berdasarkan mahasiswa_id
         $groupedData = $dataScreening
