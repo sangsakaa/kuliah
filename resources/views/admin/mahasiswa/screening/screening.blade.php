5d66ed338663917be2f5ac38e1c1fb6bfe59db94
sangsaka <zainuri717@gmail>
2024-06-20 08:50:13 +0700

    git

--- resources/views/admin/mahasiswa/screening/screening.blade.php
+++ resources/views/admin/mahasiswa/screening/screening.blade.php
@@ -192,7 +192,6 @@
           @if(count($dataScreening) > 0)
           @foreach($dataScreening as $item)
           <div class=" grid grid-cols-1 text-xs">
-
             <div>
               {{ $item->nim }} :
               {{ $item->nama_mhs }}
@@ -205,7 +204,6 @@
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
                 </svg>
-                Valid sesui format
               </span>
               @elseif ($item->status_file == 'Invalid')
               <!-- Display a red cross icon -->
@@ -213,13 +211,12 @@
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                 </svg>
-                Dokumen diTolak
+
               </span>
               @else
               <!-- Display a message indicating the file has not been uploaded -->
               <span class="text-gray-500">File sudah di-upload</span>
               @endif
-
             </div>
           </div>
           @endforeach
